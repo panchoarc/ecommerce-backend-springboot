e84ad368a5718350c64a55cc9d6746709ac720e7
Francisco Ríos <francisco.rios.cabello@gmail.com>
2025-03-08 19:06:21 -0300

    Added Localstack for test to emulate AWS services

--- src/test/java/com/buyit/ecommerce/config/TestContainersConfig.java
+++ src/test/java/com/buyit/ecommerce/config/TestContainersConfig.java
@@ -5,26 +5,31 @@ import com.github.dockerjava.api.model.PortBinding;
 import com.github.dockerjava.api.model.Ports;
 import dasniko.testcontainers.keycloak.KeycloakContainer;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.MountableFile;
 
 import java.time.Duration;
 import java.util.Objects;
 
+import static org.testcontainers.containers.localstack.LocalStackContainer.Service.*;
+
 
 @Slf4j
+@Testcontainers
 public class TestContainersConfig {
 
 
     private static final String POSTGRES_IMAGE_VERSION = "postgres:15-alpine";
     private static final String KEYCLOAK_IMAGE_VERSION = "quay.io/keycloak/keycloak:26.0";
+    private static final String LOCALSTACK_IMAGE_VERSION = "localstack/localstack:4";
 
     @Container
     @ServiceConnection
@@ -32,7 +37,6 @@ public class TestContainersConfig {
             .withDatabaseName("ecommerce")
             .withUsername("admin")
             .withPassword("admin")
-            .withInitScript("init-data.sql")
             .waitingFor(Wait.forListeningPort().withStartupTimeout(Duration.ofMinutes(3)));
 
 
@@ -47,15 +51,21 @@ public class TestContainersConfig {
             ));
 
 
-    @BeforeAll
-    static void beforeAll() {
-        postgreSQLContainer.start();
-        keycloakContainer.start();
-    }
+    @Container
+    static LocalStackContainer localStackContainer = new LocalStackContainer(DockerImageName.parse(LOCALSTACK_IMAGE_VERSION))
+            .withExposedPorts(4566)
+            .withServices(S3, SQS, DYNAMODB)
+            .withReuse(true);
 
 
     @DynamicPropertySource
     static void setProperties(DynamicPropertyRegistry registry) {
         registry.add("keycloak.auth-server-url", keycloakContainer::getAuthServerUrl);
+        registry.add("aws.s3.bucket-name", () -> "my-test-bucket");
+        registry.add("aws.s3.endpoint", () -> localStackContainer.getEndpointOverride(S3).toString());
+        registry.add("aws.s3.region", () -> localStackContainer.getRegion());
+        registry.add("aws.s3.access-key", localStackContainer::getAccessKey);
+        registry.add("aws.s3.secret-key", localStackContainer::getSecretKey);
+
     }
 }
