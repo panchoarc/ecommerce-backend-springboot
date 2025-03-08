2396f4b5f5044def940105e5cf2a779c0d90a588
Francisco Ríos <francisco.rios.cabello@gmail.com>
2025-03-08 19:09:39 -0300

    Changed ProductController.java to use @RequestPart instead of @ModelAttribute for catching multipartFile

--- src/main/java/com/buyit/ecommerce/controller/ProductController.java
+++ src/main/java/com/buyit/ecommerce/controller/ProductController.java
@@ -1,6 +1,5 @@
 package com.buyit.ecommerce.controller;
 
-import com.buyit.ecommerce.dto.request.product.CreateProductImagesRequest;
 import com.buyit.ecommerce.dto.request.product.CreateProductRequest;
 import com.buyit.ecommerce.dto.request.product.ProductRequest;
 import com.buyit.ecommerce.dto.request.product.UpdateProductRequest;
@@ -19,8 +18,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
@@ -75,11 +74,11 @@ public class ProductController {
     }
 
 
-    @PostMapping(value = "/{id}/images", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/{id}/images")
     @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<Void> addImagesToProduct(@PathVariable("id") Long id,
-                                                @Valid @ModelAttribute CreateProductImagesRequest images) throws IOException {
-        return productImageService.uploadProductImage(images.getImages(), id);
+                                                @Valid @RequestPart("images") MultipartFile[] images) throws IOException {
+        return productImageService.uploadProductImage(images, id);
     }
 
     @GetMapping(value = "/{id}/images")
