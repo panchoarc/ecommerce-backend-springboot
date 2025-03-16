9bbf6704f9689d870471293c98f4d08898dcca43
Francisco Ríos <francisco.rios.cabello@gmail.com>
2025-03-16 16:48:01 -0300

    Renamed class to webconfig in order to not only add Cors

--- src/main/java/com/buyit/ecommerce/config/WebConfig.java
+++ src/main/java/com/buyit/ecommerce/config/WebConfig.java
@@ -5,7 +5,8 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements  WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
+
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
