dc8b97b6eca4e70e6a3771b00b0a5645d7dafb0a
Francisco Ríos <francisco.rios.cabello@gmail.com>
2025-03-17 01:35:54 -0300

    Fixed config-aws.sh to export variables

--- localstack/scripts/config-aws.sh
+++ localstack/scripts/config-aws.sh
@@ -6,7 +6,11 @@ AWS_ENDPOINT="http://s3.localhost.localstack.cloud:4566"
 AWS_ACCESS_KEY_ID="test"
 AWS_SECRET_ACCESS_KEY="test"
 
+# Configura las credenciales
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 
+# Configura el endpoint de S3 en LocalStack
 awslocal configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
 awslocal configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
 awslocal configure set region us-east-1
