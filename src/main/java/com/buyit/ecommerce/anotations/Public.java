495683e9c9d383f106e095df81d6206fadbb17e9
Francisco Ríos <francisco.rios.cabello@gmail.com>
2025-03-26 15:04:28 -0300

    Added annotation to make a public endpoint

---
+++ src/main/java/com/buyit/ecommerce/anotations/Public.java
@@ -0,0 +1,13 @@
+package com.buyit.ecommerce.anotations;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE}) // Puede aplicarse a métodos y clases
+public @interface Public {
+}
+
