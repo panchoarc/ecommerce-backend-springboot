51a661fc768a1eced780a17e01d431c6b8e37f63
Francisco Ríos <panchorios_242@hotmail.com>
2025-07-13 18:18:18 -0400

    Added method for similar products

--- src/main/java/com/buyit/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/buyit/ecommerce/repository/ProductRepository.java
@@ -7,8 +7,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -18,4 +21,16 @@ public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpec
     Optional<Product> findByName(String name);
 
     boolean existsByNameAndProductIdNot(@NotBlank(message = "name cannot be blank") String name, Long id);
+
+
+    @Query("""
+                SELECT p FROM Product p
+                JOIN ProductCategory pc ON p = pc.product
+                WHERE pc.category.categoryId = :categoryId
+                AND p.productId <> :productId
+                AND p.price BETWEEN :minPrice AND :maxPrice
+                AND pc.isActive = true
+                ORDER BY p.price ASC
+            """)
+    List<Product> findTop10SimilarProducts(Long categoryId, BigDecimal minPrice, BigDecimal maxPrice, Long productId, Pageable pageable);
 }
