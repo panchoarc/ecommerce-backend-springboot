f90c87bc1a1c8539c8d089946119be35163d5ef1
Francisco Ríos <panchorios_242@hotmail.com>
2026-05-31 20:21:29 -0400

    Fixed scripts to be compatible with ministack

---
+++ init.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE keycloak;
+CREATE DATABASE ecommerce_db;
--- localstack/scripts/create-bucket.sh
+++ localstack/scripts/create-bucket.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-echo "===> [S3] Creando bucket en LocalStack..."
+echo "===> [S3] Creando bucket en Ministack..."
 
-# Configuración
 BUCKET_NAME="ecommerce-buyit-bucket"
-AWS_ENDPOINT="http://s3.localhost.localstack.cloud:4566"
+ENDPOINT="http://localhost:4566"
 
 # Crear bucket
-awslocal --endpoint-url=${AWS_ENDPOINT} s3api create-bucket --bucket ${BUCKET_NAME}
+awslocal --endpoint-url=$ENDPOINT s3api create-bucket \
+  --bucket $BUCKET_NAME
 
 # Configurar CORS
-echo "===> [S3] Configurando CORS para el bucket..."
-awslocal --endpoint-url=${AWS_ENDPOINT} s3api put-bucket-cors \
-  --bucket ${BUCKET_NAME} \
-  --cors-configuration file:///etc/localstack/init/ready.d/cors.json
-echo "===> [S3] CORS configurado correctamente."
+echo "===> [S3] Configurando CORS..."
+awslocal --endpoint-url=$ENDPOINT s3api put-bucket-cors \
+  --bucket $BUCKET_NAME \
+  --cors-configuration file:///etc/ministack/init/ready.d/cors.json
 
 # Listar buckets
-echo "===> [S3] Buckets existentes:"
-aws --endpoint-url=${AWS_ENDPOINT} s3api list-buckets
+echo "===> [S3] Buckets:"
+awslocal --endpoint-url=$ENDPOINT s3api list-buckets
 
-# Mostrar URL
-echo "===> [S3] Bucket disponible en:"
-echo "http://${BUCKET_NAME}.s3.localhost.localstack.cloud:4566"
+echo "===> [S3] OK"
